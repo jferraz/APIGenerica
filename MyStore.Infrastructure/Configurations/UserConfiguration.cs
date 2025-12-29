d7bd3f468ab985ef06fa34dd23499a8436476c97
Jorge <jorge.borges@parceiro.gruposaltaedu.com>
2025-12-29 20:14:59 -0300

    Applying migrations and db creation

--- MyStore.Infrastructure/Configurations/UserConfiguration.cs
+++ MyStore.Infrastructure/Configurations/UserConfiguration.cs
@@ -6,7 +6,7 @@ namespace MyStore.Infrastructure.Configurations
 {
     public class UserConfiguration : IEntityTypeConfiguration<User>
     {
-        public void Configuration(EntityTypeBuilder<User> builder)
+        public void Configure(EntityTypeBuilder<User> builder)
         {
             builder.ToTable("Users");
 

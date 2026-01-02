711786ed36e70a1aaed80170b46276e8445ae26a
Jorge <jorge.borges@parceiro.gruposaltaedu.com>
2026-01-02 18:29:21 -0300

    Code improvements

--- MyStore.Application/Products/IProductService.cs
+++ MyStore.Application/Products/IProductService.cs
@@ -1,10 +1,11 @@
-﻿using MyStore.Domain.Entities;
+﻿using MyStore.Application.Dtos;
+using MyStore.Domain.Entities;
 
 namespace MyStore.Application.Products
 {
     public interface IProductService
     {
-       Task CreateAsync(Product product);
-       Task<IEnumerable<Product>> GetAllAsync();        
+       Task CreateAsync(CreateProductDto product);
+       Task<IEnumerable<ProductDto>> GetAllAsync();        
     }
 }
